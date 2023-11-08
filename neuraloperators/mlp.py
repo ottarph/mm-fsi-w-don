54d491a9863db41ed223adf517d357dfaa12d0c5
Ottar Hellan <hellanottar@simula.no>
2023-11-08 15:52:46 +0100

    Added SplitAdditive as a layer as well as encoder, to better separate responsibilities.

--- neuraloperators/mlp.py
+++ neuraloperators/mlp.py
@@ -26,5 +26,24 @@ class MLP(nn.Module):
     def __call__(self, x: torch.Tensor) -> torch.Tensor: # Hack to make type hint for self(x) be tensor
         return super().__call__(x)
 
+
+class SplitAdditive(nn.Module):
+
+    def __init__(self, module_1: nn.Module, module_2: nn.Module, length_1: int, length_2: int):
+        super().__init__()
+
+        self.module_1 = module_1
+        self.module_2 = module_2
+
+        self.length_1 = length_1
+        self.length_2 = length_2
+
+        return
     
-__all__ = ["MLP"]
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return super().__call__(x)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.module_1(x[...,:self.length_1]) + self.module_2(x[...,self.length_1:])
+
+__all__ = ["MLP", "SplitAdditive"]
