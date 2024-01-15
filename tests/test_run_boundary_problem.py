d0d6798f53a462acf9911fead4967405f513024b
Ottar Hellan <hellanottar@simula.no>
2024-01-15 18:50:42 +0100

    Fixed broken test

--- tests/test_run_boundary_problem.py
+++ tests/test_run_boundary_problem.py
@@ -10,7 +10,7 @@ def test_run_boundary_problem(tmp_path):
     with open(default_problem_path, "r") as infile:
         default_probdict = json.loads(infile.read())
     
-    default_probdict["num_epochs"] = 3
+    default_probdict["num_epochs"] = 1
 
     tmp_prob_dict_path = tmp_path / "prob_dict.json"
     with open(tmp_prob_dict_path, "w") as outfile:
@@ -18,14 +18,22 @@ def test_run_boundary_problem(tmp_path):
 
     tmp_results_dir = tmp_path / "results"
     tmp_results_dir.mkdir()
+    latest_results_dir = tmp_path / "latest"
+    latest_results_dir.mkdir()
 
-    run_boundary_problem(tmp_prob_dict_path, tmp_results_dir, save_xdmf=False, latest_results_dir=tmp_path / "latest")
+    run_boundary_problem(tmp_prob_dict_path, tmp_results_dir, save_xdmf=False, latest_results_dir=latest_results_dir)
 
-    (tmp_results_dir / "state_dict.pt").unlink()
+    (tmp_results_dir / "branch.pt").unlink()
+    (tmp_results_dir / "trunk.pt").unlink()
     (tmp_results_dir / "train_val_lr_hist.pdf").unlink()
 
-    valstr = (tmp_results_dir / "data/val.txt").read_text()
+    (latest_results_dir / "branch.pt").unlink()
+    (latest_results_dir / "trunk.pt").unlink()
+    (latest_results_dir / "train_val_lr_hist.pdf").unlink()
 
-    assert len(valstr.split("\n")) == 4
+    valstr = (tmp_results_dir / "data/val.txt").read_text()
+    assert len(valstr.split("\n")) == 2
+    valstr = (latest_results_dir / "data/val.txt").read_text()
+    assert len(valstr.split("\n")) == 2
 
     return
