7b8fff37bfe43faec7aa3f0f522bb35a630eeaed
Ottar Hellan <hellanottar@simula.no>
2024-01-23 11:21:15 +0100

    Timing of deeponet components to text file for different mesh sizes.

--- deeponet_timing/component_timing.py
+++ deeponet_timing/component_timing.py
@@ -7,11 +7,13 @@ from tqdm import tqdm
 
 import itertools
 
-mesh_mult = 20
+mesh_mult = 1
+num_rep = min(1000, int(1000 / mesh_mult))
 dset_b = torch.rand((1000, int(3935*mesh_mult), 2))
 dset_t = torch.rand((1000, int(3935*mesh_mult), 2))
 
 print(f"{mesh_mult = }")
+print(f"{num_rep = }")
 
 start = timer()
 for k in range(len(dset_b)):
@@ -50,11 +52,12 @@ b, t = next(dliter)
 
 print(f"{b.shape = }")
 print(f"{t.shape = }")
-print()
+
+print("------+------+------+------+------+------+------+------")
 
 Tot = 0.0
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
     benc = branch_encoder(b)
@@ -62,7 +65,7 @@ end = timer()
 print(f"CPU: branch_encoder(b): {(end-start)/N:.2e}")
 Tot += (end-start)/N
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
     tenc = trunk_encoder(t)
@@ -70,7 +73,7 @@ end = timer()
 print(f"CPU: trunk_encoder(t): {(end-start)/N:.2e}")
 Tot += (end-start)/N
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
     bk = branch(benc)
@@ -78,7 +81,7 @@ end = timer()
 print(f"CPU: branch(benc): {(end-start)/N:.2e}")
 Tot += (end-start)/N
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
     tk = trunk(tenc)
@@ -86,7 +89,7 @@ end = timer()
 print(f"CPU: trunk(tenc): {(end-start)/N:.2e}")
 Tot += (end-start)/N
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
     dn = deeponet.combine(bk, tk)
@@ -96,7 +99,7 @@ Tot += (end-start)/N
 
 print(f"CPU: Tot: {Tot:.2e}")
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
     dn = deeponet(b, t)
@@ -105,6 +108,7 @@ print(f"CPU: deeponet(b, t): {(end-start)/N:.2e}")
 
 print("------+------+------+------+------+------+------+------")
 
+torch.cuda.empty_cache()
 deeponet.to("cuda")
 filter_tens = filter_tens.to("cuda")
 deeponet.branch_encoder = SequentialEncoder(FixedFilterEncoder(filter_tens, dim=-2, unit_shape_length=2), FlattenEncoder(start_dim=-2))
@@ -115,35 +119,41 @@ b, t = next(dliter)
 
 Tot = 0.0
 
+torch.cuda.empty_cache()
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
-    b_c, t_c = b.to("cuda"), t.to("cuda")
+    b_c= b.to("cuda")
 end = timer()
-print(f"GPU: to(\"cuda\"): {(end-start)/N:.2e}")
-# Tot += (end-start)/N
-
-
-N = 10
-start = timer()
-for _ in range(N):
-    b_c = b.to("cuda")
-    benc = branch_encoder(b_c)
-end = timer()
-print(f"GPU: branch_encoder(b)+to(\"cuda\"): {(end-start)/N:.2e}")
+print(f"GPU: b.to(\"cuda\"): {(end-start)/N:.2e}")
 Tot += (end-start)/N
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
     t_c = t.to("cuda")
-    tenc = trunk_encoder(t_c)
 end = timer()
-print(f"GPU: trunk_encoder(t)+to(\"cuda\"): {(end-start)/N:.2e}")
+print(f"GPU: t.to(\"cuda\"): {(end-start)/N:.2e}")
 Tot += (end-start)/N
 
-N = 10
+N = num_rep
+start = timer()
+for _ in range(N):
+    benc = branch_encoder(b_c)
+end = timer()
+print(f"GPU: branch_encoder(b): {(end-start)/N:.2e}")
+Tot += (end-start)/N
+
+N = num_rep
+start = timer()
+for _ in range(N):
+    tenc = trunk_encoder(t_c)
+end = timer()
+print(f"GPU: trunk_encoder(t): {(end-start)/N:.2e}")
+Tot += (end-start)/N
+
+N = num_rep
 start = timer()
 for _ in range(N):
     bk = branch(benc)
@@ -151,7 +161,7 @@ end = timer()
 print(f"GPU: branch(benc): {(end-start)/N:.2e}")
 Tot += (end-start)/N
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
     tk = trunk(tenc)
@@ -159,20 +169,25 @@ end = timer()
 print(f"GPU: trunk(tenc): {(end-start)/N:.2e}")
 Tot += (end-start)/N
 
-N = 10
+N = num_rep
 start = timer()
 for _ in range(N):
     dn = deeponet.combine(bk, tk)
+    # dn = deeponet.combine_2(bk, tk)
 end = timer()
 print(f"GPU: deeponet.combine(bk, tk): {(end-start)/N:.2e}")
+# print(f"GPU: deeponet.combine_2(bk, tk): {(end-start)/N:.2e}")
 Tot += (end-start)/N
 
 print(f"GPU: Tot: {Tot:.2e}")
 
-N = 10
+torch.cuda.empty_cache()
+N = num_rep
 start = timer()
 for _ in range(N):
     b_c, t_c = b.to("cuda"), t.to("cuda")
     dn = deeponet(b_c, t_c)
 end = timer()
 print(f"GPU: deeponet(b, t)+to(\"cuda\"): {(end-start)/N:.2e}")
+
+print("\n")
